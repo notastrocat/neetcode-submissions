8ac62a94e5b15c70a13dadc2bd9154a0a10e7cbc
Astro Cat <archit42.sharma@gmail.com>
2026-05-04 21:05:27 +0530

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.go
@@ -0,0 +1,27 @@
+func dailyTemperatures(temperatures []int) []int {
+	maxLen := len(temperatures)
+	res := make([]int, maxLen)
+	var stack []int
+
+	for i,v := range temperatures {
+		for len(stack) > 0 && v > temperatures[stack[len(stack)-1]] {
+			// pop from stack
+			last := len(stack)-1
+			top := stack[last]
+			stack = stack[:last]
+
+			// store the diff
+			res[top] = i - top
+		}
+
+		stack = append (stack, i)
+	}
+
+	return res
+}
+
+// very naïve way to solve would be to iterate over and over.
+// [30,38,30,36,35,40,28]
+// [ 0, 1, 2, 3, 4, 5, 6 ]
+
+// [ 1, 4, 1, 2, 1, 0, 0 ]
